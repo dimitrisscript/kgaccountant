e3007e212f05eb128540e933c19cde804a5502d5
Dimitris Siakavelis <dimitrisscript@gmail.com>
2021-03-21 00:49:46 +0100

    add logo

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,20 +9,7 @@
           aria-label="home"
           class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            class="w-10 h-10 text-white p-2 bg-indigo-700 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
-            ></path>
-          </svg>
+          <img src="/logo.svg" class="h-8" />
         </NuxtLink>
         <p class="text-sm text-gray-800 sm:ml-6 sm:mt-0 mt-4">
           © 2020 {{ $attrs.data.title }} —
